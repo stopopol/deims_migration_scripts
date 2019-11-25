146be8f05d84aeecb9ea09c11e1122b42160a6fe
stopopol <christoph.wohner@umweltbundesamt.at>
2019-11-25 09:07:23 +0100

    Update boundaries_sql_query.sql

--- boundaries_sql_query.sql
+++ boundaries_sql_query.sql
@@ -1,7 +1,3 @@
-	
-	
-	
-	
 UPDATE drupal8.node__field_boundaries 
 SET    
 	field_boundaries_value = ( 
@@ -13,19 +9,45 @@ SET
 		SELECT `field_geo_bounding_box_geo_type`
 		FROM deims7_prod.`field_data_field_geo_bounding_box` 
 		WHERE `entity_id` = 7968
+	),
+	field_boundaries_geo_lat = ( 
+		SELECT `field_geo_bounding_box_lat`
+		FROM deims7_prod.`field_data_field_geo_bounding_box` 
+		WHERE `entity_id` = 7968
+	),
+	field_boundaries_geo_lon = ( 
+		SELECT `field_geo_bounding_box_lon`
+		FROM deims7_prod.`field_data_field_geo_bounding_box` 
+		WHERE `entity_id` = 7968
+	),
+	field_boundaries_geo_left = ( 
+		SELECT `field_geo_bounding_box_left`
+		FROM deims7_prod.`field_data_field_geo_bounding_box` 
+		WHERE `entity_id` = 7968
+	),
+	field_boundaries_geo_top = ( 
+		SELECT `field_geo_bounding_box_top`
+		FROM deims7_prod.`field_data_field_geo_bounding_box` 
+		WHERE `entity_id` = 7968
+	),
+	field_boundaries_geo_right = ( 
+		SELECT `field_geo_bounding_box_right`
+		FROM deims7_prod.`field_data_field_geo_bounding_box` 
+		WHERE `entity_id` = 7968
+	),
+	field_boundaries_geo_bottom = ( 
+		SELECT `field_geo_bounding_box_bottom`
+		FROM deims7_prod.`field_data_field_geo_bounding_box` 
+		WHERE `entity_id` = 7968
+	),
+	field_boundaries_geo_geohash = ( 
+		SELECT `field_geo_bounding_box_geohash`
+		FROM deims7_prod.`field_data_field_geo_bounding_box` 
+		WHERE `entity_id` = 7968
 	)
 
 WHERE `entity_id` = 7968
 	
 	
-	-- TO DO;
-		--`field_geo_bounding_box_lat`,
-		--`field_geo_bounding_box_lon`,
-		--`field_geo_bounding_box_left`,
-		--`field_geo_bounding_box_top`,
-		--`field_geo_bounding_box_right`,
-		--`field_geo_bounding_box_bottom`,
-		--`field_geo_bounding_box_geohash`
-		
-		
-    -- and join based on entity_id
+	-- TO DO;		
+    -- and join based on entity_id?
